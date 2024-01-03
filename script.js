c117d3147452c1f80b56afec441645cf94474d79
Linfindel <linfindel@gmail.com>
2024-01-03 21:04:40 +0000

    Style improvements

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let words = [];
 let submittedWords = [];
 
 fetch("https://gist.githubusercontent.com/bmschmidt/7a903432d42606ead4c22b677646df1a/raw/0feb5962658ac8b92c6a33971973357333b6fe53/wordlist.txt")
-  .then(response => response.text())
-  .then(data => {
-    // Split the fetched text into an array of words
-    words = data.split("\n");
-  })
-  .catch(error => {
-    console.error("Error fetching the word list:", error);
-  });
+.then(response => response.text())
+.then(data => {
+  // Split the fetched text into an array of words
+  words = data.split("\n");
+})
+.catch(error => {
+  console.error("Error fetching the word list:", error);
+});
 
 document.getElementById("input").addEventListener("input", () => {
   const input = document.getElementById("input").value.trim(); // Trim whitespace
